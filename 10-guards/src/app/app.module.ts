14ab7f31938fcc89c48f7c4d87193bc54804ebc9
Diego Santana <3c.santana@gmail.com>
2024-01-12 23:34:41 -0300

    Criando os componentes home, produtos e sobre e config as rotas

--- 10-guards/src/app/app.module.ts
+++ 10-guards/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
